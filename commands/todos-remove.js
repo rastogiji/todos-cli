3f29016aa6b3fae9681b4a3b1e6a9d2bd1ad55a4
Animesh Rastogi <animesh.rastogi54@gmail.com>
2022-05-25 22:06:49 +0530

    Added Remove module

--- commands/todos-remove.js
+++ commands/todos-remove.js
@@ -1,10 +1,33 @@
 const conf = require("conf");
+const inquirer = require("inquirer");
 
 const config = new conf();
 
 const remove = () => {
-  config.delete("todosList");
-  console.log("Hellow from Delete");
+  let list = config.get("todosList");
+  if (list && list.length) {
+    inquirer
+      .prompt([
+        {
+          name: "index",
+          message: "Enter the index of the task you want to remove: "
+            .brightBlue,
+          type: "input",
+        },
+      ])
+      .then((answer) => {
+        if (answer.index < list.length) {
+          list.splice(answer.index, 1);
+          config.set("todosList", list);
+          console.log("Task Removed Successfully".bold.brightGreen);
+        } else {
+          console.log(
+            `Please enter an index between 0 and ${list.length - 1}`.bold
+              .brightYellow
+          );
+        }
+      });
+  }
 };
 
 module.exports = remove;
